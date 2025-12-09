86ecc8977d038029468db044d5789f0de535f312
Dafne Hernandez <mcflurrycerezita@gmail.com>
2025-12-09 05:07:26 -0600

    excersise 4 1/2

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 import { printOutString } from "./modules/exercise1.js";
 import { transformDoubleValue } from "./modules/exercise2.js"
 import { sumAndProduct } from "./modules/exercise3.js";
+import { isMatch } from "./modules/exercise4.js";
 
-//printOutString();       //exercise 1
-//transformDoubleValue(); //exercise 2
-sumAndProduct();        //exercise 3
+let input1Courses = prompt("Enter first array, put words separated by spaces:");
+let input2Courses = prompt("Enter second array, put words separated by spaces:");
+// Convertimos strings 
+let student1Courses = input1Courses.split(" ");
+let student2Courses = input2Courses.split(" ");
+
+//printOutString();                                     //exercise 1
+//transformDoubleValue();                               //exercise 2
+//sumAndProduct();                                      //exercise 3
+const msg = isMatch(student1Courses, student2Courses);  //exercise 4
+console.log(msg);
--- src/js/modules/exercise4.js
+++ src/js/modules/exercise4.js
@@ -3,3 +3,40 @@
     let student2Courses = ['Geography', 'Spanish', 'Programming'];
 Create a program that loops over the 2 arrays; if there are any common courses print them out to the console.
  */
+
+function isMatch(firstCourses,secondCourses){
+    const resultArray = []; 
+    for(const course1 of firstCourses){
+        for(const course2 of secondCourses){
+            if (course1 === course2){
+                resultArray.push(course1); 
+                //console.log(`The course they have in common is ${resultArray}`);
+            }
+        }
+    }
+    
+    if (resultArray.length === 0) {
+        return "There are no courses in common.";
+    }
+
+    if (resultArray.length === 1) {
+        return `The course they have in common is ${resultArray[0]}`;
+    }
+
+    return `The courses they have in common are: ${resultArray.join(", ")}`;
+}
+
+/*
+//let student1Courses = ['Math', 'English', 'Programming'];
+//let student2Courses = ['Geography', 'Spanish', 'Programming'];
+
+let input1Courses = prompt("Enter first array, put words separated by spaces:");
+let input2Courses = prompt("Enter second array, put words separated by spaces:");
+
+/ Convertimos strings
+let student1Courses = input1Courses.split(" ");
+let student2Coursess = input2Courses.split(" ");
+*/
+//isMatch(input1Courses, input2Courses);
+
+export {isMatch}
