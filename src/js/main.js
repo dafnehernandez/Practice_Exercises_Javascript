3ac3cd583391506ce8e009187d8e5b2b96be58f1
Dafne Hernandez <mcflurrycerezita@gmail.com>
2025-12-09 21:58:57 -0600

    feat: exercise 6 complete

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import { transformDoubleValue } from "./modules/exercise2.js"
 import { sumAndProduct } from "./modules/exercise3.js";
 import { courseMatch } from "./modules/exercise4.js";
 import { listingPeople } from "./modules/exercise5.js";
+import { bubbleSortInteractive } from "./modules/exerciseBubbleSort.js";
 
-printOutString();        //exercise 1
-transformDoubleValue();  //exercise 2
-sumAndProduct();         //exercise 3
-courseMatch();           //exercise 4
-listingPeople();         //exercise 5
+printOutString();         //exercise 1
+transformDoubleValue();   //exercise 2
+sumAndProduct();          //exercise 3
+courseMatch();            //exercise 4
+listingPeople();          //exercise 5
+bubbleSortInteractive (); //exercise 6
--- src/js/modules/exerciseBubbleSort.js
+++ src/js/modules/exerciseBubbleSort.js
@@ -4,39 +4,67 @@ Input [3, 6, 12, 5, 100, 1]
 Output [1, 3, 5, 6, 12, 100]
  */
 
-function bubbleSort(inputArray) {
+export function bubbleSort(inputArray) {
 
-  let swapped = true;  // enter on the cycle
+  let swapped = true; // enter on the cycle
 
   while (swapped) {
-    swapped = false;   // assume there will be no exchanges
+    swapped = false;  // assume there will be no exchanges
 
     for (let i = 0; i < inputArray.length - 1; i++) {
-
       if (inputArray[i] > inputArray[i + 1]) {
-        
+
         //Swapping using a temporary variable
         let temp = inputArray[i];
         inputArray[i] = inputArray[i + 1];
         inputArray[i + 1] = temp;
 
-        swapped = true; 
+        swapped = true;
       }
-
     }
-
   }
 
   return inputArray;
 }
 
-/* Testing
-let myArray = [5, 1, 4, 2, 8];
-let sortedArray = bubbleSort(myArray);  
-console.log("Sorted array:", sortedArray); // Output: [1, 2, 4, 5, 8]  
-*/
+export function bubbleSortInteractive() {
 
-let myArray = [3, 6, 12, 5, 100, 1];
-console.log("Input array:", myArray);
-let sortedArray = bubbleSort(myArray);  
-console.log("Sorted array:", sortedArray); // Sorted array: [1, 3, 5, 6, 12, 100]
+  let error = true;
+
+  while (error) {
+
+    let option = Number(prompt("Select 1 to see an example or 2 to enter your own array:\n"));
+
+    switch (option) {
+
+      case 1: {
+        let exampleArray = [3, 6, 12, 5, 100, 1];
+        console.log("Original array:", exampleArray);
+
+        let sorted = bubbleSort(exampleArray);
+        console.log("Sorted array:", sorted); // Sorted array: [1, 3, 5, 6, 12, 100]
+
+        error = false;
+        break;
+      }
+
+      case 2: {
+        let input = prompt("Enter numbers separated by spaces:");
+        let inputArray = input.split(" ").map(Number);
+
+        console.log("Your array:", inputArray);
+
+        let sorted = bubbleSort(inputArray);
+        console.log("Sorted array:", sorted);
+
+        error = false;
+        break;
+      }
+
+      default:
+        console.log("Error, please select 1 or 2");
+        break;
+    }
+  }
+}
+
